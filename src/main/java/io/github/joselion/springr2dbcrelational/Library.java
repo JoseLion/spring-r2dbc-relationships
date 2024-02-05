b273b4e3a034b7a013a1aef67cd325842969d9a3
Jose Luis Leon <joseluis5000l@gmail.com>
2024-02-05 02:29:07 -0500

    feat(core): Project bootstrap (#1)

---
+++ src/main/java/io/github/joselion/springr2dbcrelational/Library.java
@@ -0,0 +1,16 @@
+package io.github.joselion.springr2dbcrelational;
+
+/**
+ * Auto-generetaed test class.
+ */
+public record Library() {
+
+  /**
+   * Auto-generetaed test method.
+   *
+   * @return always true
+   */
+  public boolean someLibraryMethod() {
+    return true; // NOSONAR
+  }
+}
